8663a78c493e27e09fa4f3d9493a9ef4e76cbd7e
Igor Todorovic <igor333m@yahoo.com>
2017-11-11 18:04:44 -0800

    test: Add test for loading feeds

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -13,7 +13,7 @@ $(function() {
     * a related set of tests. This suite is all about the RSS
     * feeds definitions, the allFeeds variable in our application.
     */
-    describe('RSS Feeds', function() {
+    describe("RSS Feeds", function() {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
          * empty. Experiment with this before you get started on
@@ -21,22 +21,39 @@ $(function() {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
-        it('are defined', function() {
+         let feed;
+         beforeEach( () => {
+            allFeeds.forEach( (obj) => {
+                feed = obj;
+            });
+         });
+         
+        it("are defined", function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
-
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
-
+        it("URL-s are defined and not empty", () => {
+            // allFeeds.forEach( (obj) => {
+            //     expect(obj.url).toBeDefined();
+            //     expect(obj.url.length).not.toBe(0);
+            // });
+            expect(feed.url).toBeDefined();
+            expect(feed.url.length).not.toBe(0);
+        });
+        
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it("names are defined and not empty", () => {
+            expect(feed.name).toBeDefined();
+            expect(feed.name.length).not.toBe(0);
+         });
     });
 
 
